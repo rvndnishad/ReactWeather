fc47e3b03a6d8877fa82c325eec3d692e9c8c24e
Arvind Nishad <rvndnishad@gmail.com>
2016-10-28 11:40:15 +0530

    Add nav search support

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,13 @@ var { Link, IndexLink }  = require('react-router');
 var Nav = React.createClass({
   onSearch: function(e){
     e.preventDefault;
-    alert("Not yet wired up.")
+    var location = this.refs.search.value;
+    var encodedLocation = encodeURIComponent(location);
+
+    if(location.length > 0) {
+      this.refs.search.value='';
+      window.location.hash='#/?location='+encodedLocation;
+    }
   },
   render: function(){
     return(
@@ -21,7 +27,7 @@ var Nav = React.createClass({
           <div className="top-bar-right">
               <form action="" onSubmit={this.onSearch}>
                 <ul className="menu">
-                  <li><input type="search" placeholder="Search weather by city" ref=""/></li>
+                  <li><input type="search" placeholder="Search weather by city" ref="search"/></li>
                   <li><input type="submit" className="button" value="Get Weather"/></li>
               </ul>
               </form>
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,7 +15,9 @@ var Weather = React.createClass({
     var that = this;
     this.setState({
       isLoading:true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location:undefined,
+      temp:undefined
      });
 
     var temp = openWeatherMap.getTemp(location).then(function(temp){
@@ -30,6 +32,20 @@ var Weather = React.createClass({
         errorMessage: e.message
       });
     });
+  },
+  componentDidMount: function() {
+    var location = this.props.location.query.location;
+    if(location && location.length > 0) {
+        this.handleSearch(location);
+        window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps:function(newProps) {
+    var location = newProps.location.query.location;
+    if(location && location.length > 0) {
+        this.handleSearch(location);
+        window.location.hash = '#/';
+    }
   },
   render: function(){
     var {isLoading, temp, location, errorMessage} = this.state;
