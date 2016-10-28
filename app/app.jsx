70aa6bfe578121dc736330ceaf65d1bda223d311
Arvind Nishad <rvndnishad@gmail.com>
2016-10-28 11:09:12 +0530

    Switch to SCSS

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
 //app class
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 ReactDOM.render(
   <Router history={hashHistory}>
 			<Route path="/" component={Main}>
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       About: 'app/components/About.jsx',
       Examples: 'app/components/Examples.jsx',
       openWeatherMap : 'app/api/openWeatherMap.jsx',
-      applicationStyles:'app/styles/app.css'
+      applicationStyles:'app/styles/app.scss'
 
     },
     extensions : ['', '.js', '.jsx']
